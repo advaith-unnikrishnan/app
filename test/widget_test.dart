e68033d77271934344e46f3f098092abd8901d1f
Venkitesh S. Anand <venkiteshsanand1920@gmail.com>
2020-06-28 16:40:26 +0530

    ontap in menu icon

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wefge/main.dart';
+import 'package:lokale/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
